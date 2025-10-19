22a27ee23337be1e0e80b95dd7380ca11baf590d
Michael Reilly <OmnipotentEntity@gmail.com>
2025-10-19 10:14:57 -0500

    Implemented Magician and High Priestess

--- src/consumables/01-magician.lua
+++ src/consumables/01-magician.lua
@@ -1,10 +1,17 @@
 curt_Consumable {
   key = "rev_magician",
   atlas = "rev_tarot",
-  pos = { x = 1, y = 0 },
-
-  calculate = function()
-
-  end
+  pos = { x = 1, y = 0 }
 }
 
+local old_smods_pseudorandom_probability = SMODS.pseudorandom_probability
+
+function SMODS.pseudorandom_probability(trigger_obj, seed, base_numerator, base_denominator, identifier, no_mod)
+  local magicians = SMODS.find_card("c_curt_rev_magician") 
+  if #magicians then
+    base_numerator = base_denominator
+    magicians[1]:start_dissolve()
+  end
+  result = old_smods_pseudorandom_probability(trigger_obj, seed, base_numerator, base_denominator, identifier, no_mod)
+  return result
+end
--- src/consumables/02-high_priestess.lua
+++ src/consumables/02-high_priestess.lua
@@ -3,8 +3,23 @@ curt_Consumable {
   atlas = "rev_tarot",
   pos = { x = 2, y = 0 },
 
-  calculate = function()
-
+  calculate = function(self, card, context)
+    if context.buying_card and context.card.ability.set == "Planet" then
+      for _ = 1, 2 do
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+          local t = { 
+            set = "Planet",
+            area = G.consumeables,
+            key = context.card.config.center_key,
+            edition = {negative = true},
+          }
+          local _card = SMODS.add_card(t)
+          _card:juice_up(0.3, 0.5)
+          return true end }))
+      end
+      delay(0.6)
+      card:start_dissolve()
+    end
   end
 }
 
