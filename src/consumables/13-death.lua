1129bf43ce6759ef156cde5f09a028bdbab77c94
Michael Reilly <OmnipotentEntity@gmail.com>
2025-10-22 14:25:45 -0500

    Implement Death

--- src/consumables/13-death.lua
+++ src/consumables/13-death.lua
@@ -9,7 +9,17 @@ curt_Consumable {
   end,
 
   calculate = function(self, card, context)
+    if context.end_of_round and not game_over and context.cardarea == G.consumeables then
+      ease_ante(-card.ability.extra.antes)
+      G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante or G.GAME.round_resets.ante
+      G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante-card.ability.extra.antes
 
+      G.GAME.curt_rev_death_minus = G.GAME.curt_rev_death_minus or 1
+      G.hand:change_size(-G.GAME.curt_rev_death_minus)
+      G.GAME.curt_rev_death_minus = G.GAME.curt_rev_death_minus + 1
+
+      curt_queue_juice_use_dissolve(card)
+    end
   end
 }
 
