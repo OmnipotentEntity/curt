3601b1e53d0788b6c652e93b24ea1c8b4983d2ee
Michael Reilly <OmnipotentEntity@gmail.com>
2025-10-22 13:04:11 -0500

    Implemented Wheel of Fortune

--- src/consumables/10-wheel_of_fortune.lua
+++ src/consumables/10-wheel_of_fortune.lua
@@ -9,6 +9,28 @@ curt_Consumable {
   end,
 
   calculate = function(self, card, context)
+    if context.before then
+      for _, v in ipairs(context.scoring_hand) do
+        local edition = poll_edition('curt_rev_wheel_of_fortune_edition', nil, true, true)
+        v:set_edition(edition, true)
+        G.E_MANAGER:add_event(Event({
+          func = function()
+            v:juice_up()
+            return true
+          end
+        })) 
+        card:juice_up(0.3, 0.5)
+      end
+    end
 
+    if context.after then
+      for _, v in ipairs(context.scoring_hand) do
+        if SMODS.pseudorandom_probability(card, 'curt_rev_wheel_of_fortune_destroy', card.ability.extra.prob[1], card.ability.extra.prob[2]) then
+          SMODS.destroy_cards(v)
+        end
+      end
+
+      curt_queue_juice_use_dissolve(card)
+    end
   end
 }
