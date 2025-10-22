60c8b620e7a72861de0b1ffcbec78cc56cd0e014
Michael Reilly <OmnipotentEntity@gmail.com>
2025-10-22 13:19:14 -0500

    Implement Strength

--- src/consumables/11-strength.lua
+++ src/consumables/11-strength.lua
@@ -4,7 +4,12 @@ curt_Consumable {
   pos = { x = 5, y = 1 },
 
   calculate = function(self, card, context)
-
+    if context.pre_discard then
+      for _, v in ipairs(context.full_hand) do
+        SMODS.destroy_cards(v)
+      end
+      curt_queue_juice_use_dissolve(card)
+    end
   end
 }
 
