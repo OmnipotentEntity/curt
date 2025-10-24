505e93aacc6c71d4de4404379d526ee71b9be471
Michael Reilly <OmnipotentEntity@gmail.com>
2025-10-24 18:32:27 -0500

    Prevent multiple Empress from triggering on the same sale, fix crash

--- src/consumables/03-empress.lua
+++ src/consumables/03-empress.lua
@@ -6,14 +6,19 @@ curt_Consumable {
   calculate = function(self, card, context)
     if context.selling_card and context.card.ability.consumeable and
         not (context.card.config.center_key == 'c_curt_rev_empress') then
-      for i = 1, 3 do
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
-          local _card = copy_card(card_to_copy)
+      G.c_curt_rev_empress_triggered = true
+
+      G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+        for i = 1, 3 do
+          local _card = copy_card(context.card)
           _card:set_edition("e_negative", true)
           _card:add_to_deck()
           G.consumeables:emplace(_card)
-          return true end }))
-      end
+        end
+
+        G.c_curt_rev_empress_triggered = nil
+        return true end }))
+
       curt_queue_juice_use_dissolve(card)
     end
   end
