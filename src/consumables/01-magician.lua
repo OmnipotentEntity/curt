a223d649f2950a33d51e3b9f70be7e59a41855e7
Michael Reilly <OmnipotentEntity@gmail.com>
2025-10-25 05:18:59 -0500

    Fixed issue with magician multi-use

--- src/consumables/01-magician.lua
+++ src/consumables/01-magician.lua
@@ -8,10 +8,15 @@ local old_smods_pseudorandom_probability = SMODS.pseudorandom_probability
 
 function SMODS.pseudorandom_probability(trigger_obj, seed, base_numerator, base_denominator, identifier, no_mod)
   local magicians = SMODS.find_card("c_curt_rev_magician") 
-  if #magicians > 0 then
-    base_numerator = base_denominator
-    curt_queue_juice_use_dissolve(magicians[1])
+  for _,magician in ipairs(magicians) do
+    if not magician.ability.used then
+      base_numerator = base_denominator
+      magician.ability.used = true
+      curt_queue_juice_use_dissolve(magician)
+      break
+    end
   end
+
   result = old_smods_pseudorandom_probability(trigger_obj, seed, base_numerator, base_denominator, identifier, no_mod)
   return result
 end
